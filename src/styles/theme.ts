0305a7162573824da5ab1b3e1912e36c56024fdb
Hariel Santana <hariel.oliveira@agenciaalldigital.com>
2022-05-11 17:51:32 -0300

    feat: initial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+export default {
+  colors: {
+    type: {
+      bug: '#8CB230',
+      dark: '#58575F',
+      dragon: '#0F6AC0',
+      eletric: '#EED535',
+      fairy: '#ED6EC7',
+      fighting: '#D04164',
+      fire: '#FD7D24',
+      flying: '#748FC9',
+      ghost: '#556AAE',
+      grass: '#62B957',
+      ground: '#DD7748',
+      ice: '#61CEC0',
+      normal: '#9DA0AA',
+      poison: '#A552CC',
+      psychic: '#EA5D60',
+      rock: '#BAAB82',
+    }
+  }
+}
